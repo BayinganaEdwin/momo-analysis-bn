e73816015827c2c6160200c89ecb20cef513285e
EdwinBayingana <bayinganaedwin@gmail.com>
2025-02-18 09:56:45 +0200

    update the config.json file

--- config/config.js
+++ config/config.js
@@ -1,30 +1,72 @@
 require('dotenv').config();
 
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
+const { DATABASE_URL } = process.env;
+
+if (!DATABASE_URL) {
+  console.error('❌ DATABASE_URL is not defined. Check your .env file.');
+  process.exit(1);
+}
 
 module.exports = {
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   staging: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    port: DB_PORT,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
+
+// require('dotenv').config();
+
+// const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
+
+// module.exports = {
+//   development: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: DB_NAME,
+//     host: DB_HOST,
+//     port: DB_PORT,
+//     dialect: 'postgres',
+//   },
+//   staging: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: DB_NAME,
+//     host: DB_HOST,
+//     port: DB_PORT,
+//     dialect: 'postgres',
+//   },
+//   production: {
+//     username: DB_USERNAME,
+//     password: DB_PASSWORD,
+//     database: DB_NAME,
+//     host: DB_HOST,
+//     port: DB_PORT,
+//     dialect: 'postgres',
+//   },
+// };
